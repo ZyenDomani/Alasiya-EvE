7f8dcd46df997ca3188ff8c59d1ca79b4b586dfb
Allan <zyenathalous@bellsouth.net>
2016-08-08 13:01:21 -0500

    added notes in sql/optional about db changes for alpha server test

---
+++ sql/optional/alpha_server_changes.sql
@@ -0,0 +1,21 @@
+
+-- remove stargate to Oerse.  This will isolate the Orvanne constellation
+DELETE FROM `mapDenormalize` WHERE `itemID` = 50009622 ;
+DELETE FROM `mapJumps` WHERE `stargateID` = 50009622;
+
+
+
+-- more db changes will go here
+
+
+
+
+
+
+
+
+
+
+
+
+
