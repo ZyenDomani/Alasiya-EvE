5a11be74f2f3dd71294411451086933dcf968155
Allan <zyenathalous@bellsouth.net>
2017-03-26 10:28:11 -0500

    evilnumber rounding update changed float reporting in giveskill command to integer

--- src/eve-common/utils/EvilNumber.cpp
+++ src/eve-common/utils/EvilNumber.cpp
@@ -326,7 +326,7 @@ bool EvilNumber::get_bool()
 int64 EvilNumber::get_int()
 {
     if (mType == evil_number_float)
-        return (int64)rint(mValue.fVal);
+        return (int64)ceil(mValue.fVal);
     return mValue.iVal;
 }
 
--- src/eve-server/admin/GMCommands.cpp
+++ src/eve-server/admin/GMCommands.cpp
@@ -1062,7 +1062,7 @@ PyResult Command_giveskill(Client* who, CommandDB* db, PyServiceMgr* services, c
         character->SaveSkillHistory(skillEventGMGive, EvilTimeNow().get_double(), ownerID, skillID, level, \
                                     newPoints.get_double(), character->GetTotalSP().get_double());
 
-        sLog.White("Command::GiveSkill", "skill %u set to level %u with %.2f SP.", skillID, level, newPoints.get_float());
+        sLog.White("Command::GiveSkill", "skill %u set to level %u with %" PRIi64 " SP.", skillID, level, newPoints.get_int());
 
         return new PyString ("Skill Gifting Complete");
     } else
