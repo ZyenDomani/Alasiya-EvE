0f0d720019106a8226a71f9132bcece011579b68
Allan <zyenathalous@bellsouth.net>
2017-04-21 13:28:00 -0500

    add checks to Activate/Deactivate to correctly set module state when applying effects
    Signed-off-by: Allan <zyenathalous@bellsouth.net>

--- src/eve-server/ship/modules/ActiveModule.cpp
+++ src/eve-server/ship/modules/ActiveModule.cpp
@@ -180,7 +180,11 @@ void ActiveModule::Activate(uint16 effectID, uint32 targetID/*0*/, int16 repeat/
         return;
     }
 
-    ApplyEffect(Effects::dgmStateActive, true);
+    if (targetID)
+        ApplyEffect(Effects::dgmStateTarget, true);
+    else
+        ApplyEffect(Effects::dgmStateActive, true);
+
     ShowEffect(true, false);
 
     m_ModuleState = ModuleStates::MOD_ACTIVATED;
@@ -371,7 +375,11 @@ void ActiveModule::DeactivateCycle(bool abort/*false*/)
     if (m_ModuleState != ModuleStates::MOD_DEACTIVATING)
         return;
 
-    ApplyEffect(Effects::dgmStateActive, false);
+    if (m_targetID)
+        ApplyEffect(Effects::dgmStateTarget, false);
+    else
+        ApplyEffect(Effects::dgmStateActive, false);
+
     ShowEffect(false, abort);
 
     SetModuleState(ModuleStates::MOD_ONLINE);
