4755aa8678f99cb6bdd7c348beb7b1d9a2bb74d9
Allan <zyenathalous@bellsouth.net>
2017-07-22 11:41:12 -0500

    correct entity.sql for error

--- sql/tables/entity.sql
+++ sql/tables/entity.sql
@@ -38,7 +38,7 @@ CREATE TABLE `entity` (
   `customInfo` text,
   PRIMARY KEY (`itemID`),
   KEY `typeID` (`typeID`)
-) ENGINE=InnoDB AUTO-INCREMENT=140000000 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=140000000 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
