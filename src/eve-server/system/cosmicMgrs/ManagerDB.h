47e2e9a42efe72be84cdeba4790f110ba8eb83b9
Allan <zyenathalous@bellsouth.net>
2017-08-06 15:21:25 -0500

    update header include

--- src/eve-server/system/cosmicMgrs/ManagerDB.h
+++ src/eve-server/system/cosmicMgrs/ManagerDB.h
@@ -10,7 +10,7 @@
 #define _EVEMU_SYSTEM_COSMICMGRS_MANAGERDB_H
 
 
-#include "../../../eve-common/EVE_Scanning.h"
+#include "EVE_Scanning.h"
 #include "EntityList.h"
 #include "POD_containers.h"
 #include "system/SystemDB.h"
