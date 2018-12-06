df273ab2baf4a7f97220317737d6c20b4fd630b3
Allan <zyenathalous@bellsouth.net>
2018-12-06 03:05:48 -0600

    invTypes.sql for price updates
    Signed-off-by: Allan <zyenathalous@bellsouth.net>

--- sql/tables/invTypes.sql
+++ sql/tables/invTypes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34eb353bb24868d399bfb7477b73a75bff30e16c250f82ca9fed4d7aa61df314
-size 5319143
+oid sha256:1857bcdf0e46eae7864febf2f88a9e8b60b5ef2df42eff7d99467ca0588c4ce6
+size 5763548
