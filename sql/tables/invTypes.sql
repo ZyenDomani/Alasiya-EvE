b07d7f49cc5a6f03eb9e0c26ffd2f65ab9100091
Allan <zyenathalous@bellsouth.net>
2018-12-07 12:22:51 -0600

    another price update

--- sql/tables/invTypes.sql
+++ sql/tables/invTypes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0410c50c50fdfc2cadc704bb34e5cc30ddbaa09d2804d26d21d8bdbc66d409e9
-size 5763522
+oid sha256:b6635aa56b9d8144a50409b34adb83e352158160bc64eac79623965dbd9ebc6c
+size 5763510
