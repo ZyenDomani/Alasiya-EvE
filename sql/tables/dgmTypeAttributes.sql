12a600ac43c43fb50526114f3062aeb8409e0d8d
Allan <zyenathalous@bellsouth.net>
2018-12-15 20:06:26 -0600

    attrib updates for incorrect parameter
    Signed-off-by: Allan <zyenathalous@bellsouth.net>

--- sql/tables/dgmTypeAttributes.sql
+++ sql/tables/dgmTypeAttributes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ab76e6a40338898de85f2323f682daad355cd9c5be9ea195922ab71c6236965
-size 8898376
+oid sha256:4fd7a0691a9bfbd27169ebb2a146b9692cd1d3841629f09f5197f0a30d576f19
+size 8898403
