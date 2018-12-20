15b93aebcc6914f08a60fe88f79e1c7b32598569
Allan <zyenathalous@bellsouth.net>
2018-12-19 21:29:08 -0600

    removed mining commands file (no longer used)

--- src/eve-server/admin/MiningCommands.h
+++
@@ -1,9 +0,0 @@
-//no macroguard on purpose
-
-COMMAND( roid, Acct::Role::SPAWN,
-         "(typeID) (radius) - Spawn an asteroid with the specified type." )
-COMMAND( growbelt, Acct::Role::CONTENT,
-         "- Trigger asteroid growth in the current belt." )
-COMMAND( spawnbelt, Acct::Role::SPAWN,
-         "- Creates a new asteroid belt." )
-
