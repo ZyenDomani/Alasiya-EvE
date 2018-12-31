b75889d09e1b5d7ba04e79fbe9bcb5d054848576
Allan <zyenathalous@bellsouth.net>
2018-12-31 16:19:59 -0600

    type effects update (again)

--- sql/tables/dgmTypeEffects.sql
+++ sql/tables/dgmTypeEffects.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa71d8d44bbea8accdb58ab34c5db1d76fa40b6e555ec77102cdfcc2dca619c7
-size 569931
+oid sha256:bc9a0c79ed43297b018274906810642e0a2e5f6cdff29946e1007c8c7cf5009d
+size 569916
