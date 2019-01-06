a39440ba342240e5824259529c028b0ccad1be5d
Allan <zyenathalous@bellsouth.net>
2019-01-05 21:31:55 -0600

    groupID update for mwd this should allow client to track ab/mwd by group
    Signed-off-by: Allan <zyenathalous@bellsouth.net>

--- sql/tables/invTypes.sql
+++ sql/tables/invTypes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a3f7730b81822782acd4dc43b51a315ccdaee66edfed4497a5d6da3620ffa23
-size 5763625
+oid sha256:34b29e73a931355162a4a2fd393ec9db8f1bb744f653b651791b918b3ca2faa2
+size 5763578
