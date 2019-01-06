daff17bb7df6246d6cb0169f508fa87293a5c7e8
Allan <zyenathalous@bellsouth.net>
2019-01-05 19:47:39 -0600

    set couple attribs wrong.  this is correction.  NOT player ship (32207)
    Signed-off-by: Allan <zyenathalous@bellsouth.net>

--- sql/tables/dgmTypeAttributes.sql
+++ sql/tables/dgmTypeAttributes.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd18c2daea17ffdbb813527c7e6170f8e3e1a8e2d84cefdcd72f89af1666833
+oid sha256:94f3503cdc2373b3748407e5b792f0de30da1376f633199a34ddc7f92fb269aa
 size 8900981
