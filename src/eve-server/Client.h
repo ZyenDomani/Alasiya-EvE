17efeb3d586edd5d558a4af0ae30c75ebd73da0b
Allan <zyenathalous@bellsouth.net>
2019-09-17 11:28:26 -0500

    changed undock timer time from 2.5s to 1.5s

--- src/eve-server/Client.h
+++ src/eve-server/Client.h
@@ -49,7 +49,7 @@ enum ClientTimers {
     DefaultTimer     = 1000,
     BoardTimer       = 600,
     JumpTimer        = 500,
-    UndockTimer      = 2500,     // used to delay sending Destiny::State (client error fix)
+    UndockTimer      = 1500,     // used to delay sending Destiny::State (client error fix)
     DockingTimer     = 1000,    // Timer to delay docking (as on live)
     JumpingTimer     = 4000,    // Timer to delay jumping
     MovingTimer      = 1000,
