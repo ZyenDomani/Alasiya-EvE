0e0731d26876bb6668003182ca6e21eac4b3c7c8
Allan <allan@dev.ke5jro.net>
2020-02-17 09:08:48 -0600

    status page and todo update

--- utils/op_status.php
+++ utils/op_status.php
@@ -4,12 +4,12 @@
 <td style="text-align: center;"><h2>Estimated Progress of Listed Systems</h2></td></tr></table>
 <div id="Main_progress_bar_appearance_and_calculation" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 5px">
 <table style="background: transparent; border:3px solid blue; width:100%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td>
-<table style="background: #2222EE; width:63%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td></td></tr></table></td></tr></table>
+<table style="background: #2222EE; width:65%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td></td></tr></table></td></tr></table>
 <table style="background: transparent; width:100%"><tr><td style="text-align: center;">
 </td></tr></table></div></div><div id="Current_value_marker" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 8px">
 <table style="background: transparent; border:2px clear; width:100%; height:50px; -moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td>
-<table style="background: transparent; width:63%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right">
-<tr><td> &#160;<font color="red"><b>62.9%</b></font>&#160;&#160;&#160;&#160;
+<table style="background: transparent; width:65%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right">
+<tr><td> &#160;<font color="red"><b>64.9%</b></font>&#160;&#160;&#160;&#160;
 </td></tr></table></td></tr></table></div></div><div id="goal_value_marker" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 11px">
 <table style="background: transparent; border:2px clear; width:100%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right">
 <tr><td> <font color="red"><b>100</b></font>&#160;&#160;&#160;&#160;
@@ -20,7 +20,7 @@
 Items listed as "<font color=red>NOT IMPLEMENTED</font>" have no code for that system and the server will not respond to requests for that system.<BR>
 Items listed as "<font color=magenta>Skeleton Code Only</font>" has a basic outline coded, but limited or no processing and/or returns. The server will not respond or only partially respond to these requests.<BR>
 Items listed as "<font color=yellow>Preliminary Implementation</font>" have a rough draft of their specific code, just a step or two above skeleton code, and may not function properly or completely.</font><BR><BR>
-<font color="orange">Last Update:</font>&nbsp;&nbsp;&nbsp;<font color="green">5 February 2020</font><BR>
+<font color="orange">Last Update:</font>&nbsp;&nbsp;&nbsp;<font color="green">17 February 2020</font><BR>
 <font color="orange">Page Status:</font>&nbsp;&nbsp;&nbsp;<font color="green">Current</font><BR>
 <font color="orange">Incomplete Data:</font>&nbsp;&nbsp;&nbsp;<font color="teal">All Data Current</font><BR><BR></table>
 <p><br style="clear:both;" /></p>
@@ -1481,11 +1481,11 @@ Items listed as "<font color=yellow>Preliminary Implementation</font>" have a ro
 <h2> <span class="mw-headline" id="fxSystem"> Effects System </span></h2>
 <div id="Main_progress_bar_appearance_and_calculation" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 5px">
 <table style="background: transparent; border:3px solid blue; width:100%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td>
-<table style="background: #2222EE; width:70%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td></td></tr></table>
+<table style="background: #2222EE; width:83%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td></td></tr></table>
 </td></tr></table></div></div><div id="Current_value_marker" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 8px">
 <table style="background: transparent; border:2px clear; width:100%; height:50px; -moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr><td>
-<table style="background: transparent; width:70%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr>
-<td> &#160;<font color="red"><b>70.0</b></font>&#160;&#160;&#160;&#160;</td></tr></table>
+<table style="background: transparent; width:83%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr>
+<td> &#160;<font color="red"><b>83.1</b></font>&#160;&#160;&#160;&#160;</td></tr></table>
 </td></tr></table></div></div><div id="goal_value_marker" style="position:relative; width:100%"><div style="position:absolute; width:100%; top: 11px">
 <table style="background: transparent; border:2px clear; width:100%; height:50px;-moz-border-radius: 110px; -webkit-border-radius: 110px; border-radius: 110px; text-align:right"><tr>
 <td> <font color="red"><b>100</b></font>&#160;&#160;&#160;&#160;</td></tr></table></div></div><br /><p><br /><br /></p><p><br /></p>
@@ -1493,17 +1493,17 @@ Items listed as "<font color=yellow>Preliminary Implementation</font>" have a ro
 <tr><td style="width: 70&amp;;">Service/Action</td>
 <td style="width: 4%;">Status</td>
 <td style="width: 26%;">Comments</td></tr>
-<tr><td> - Parsing Effects</td><td class="working">80%
+<tr><td> - <font color=lime>Parsing Effects</font><BR>&nbsp;&nbsp;&nbsp;This is decoding effects data before processing can occur</td><td class="working">95%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 80%; background:#00DB00"></td>
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
-<tr><td colspan=3><font color=white><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fx Source Processing - Location or Item applying the effect</h3></font></td></tr>
-<tr><td> - <font color=lime>Process Self Effects</font><BR>&nbsp;&nbsp;&nbsp;Source is Module or Charge</td><td class="working">90%
+<td><font color=pink>Final Testing</font><BR>17Feb20</td></tr>
+<tr><td colspan=3><font color=white><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fx Source Processing - Item applying the effect</h3></font></td></tr>
+<tr><td> - <font color=lime>Process Self Effects</font><BR>&nbsp;&nbsp;&nbsp;Source is Ship, Module or Charge</td><td class="working">95%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 90%; background:#00DB00"></td>
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
+<td><font color=pink>Final Testing</font><BR>10Feb20</td></tr>
 <tr><td> - <font color=lime>Process Skill Effects</font><BR>&nbsp;&nbsp;&nbsp;Source is Skill, Implant or Booster</td><td class="working">70%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
 <tr><td style="text-align: left; height: 12px; width: 70%; background:#00DB00"></td>
@@ -1520,7 +1520,7 @@ Items listed as "<font color=yellow>Preliminary Implementation</font>" have a ro
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
 <td><font color=pink>Final Testing</font><BR>19Jun19</td></tr>
 <!--
-<tr><td> - <font color=lime>Process Gang Effects</font><BR>Fleet Booster Skills</td><td class="broken">10%
+<tr><td> - <font color=lime>Process Gang Effects</font><BR>&nbsp;&nbsp;&nbsp;Fleet Booster Skills</td><td class="broken">10%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
 <tr><td style="text-align: left; height: 12px; width: 10%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
@@ -1530,45 +1530,45 @@ Items listed as "<font color=yellow>Preliminary Implementation</font>" have a ro
 <tr><td style="text-align: left; height: 12px; width: 1%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
 <td><font color=red>NOT IMPLEMENTED</font><BR>19Jun19</td></tr>
-<tr><td> - <font color=lime>Process Target Effects</font><BR>Target Source Unknown at this time</td><td class="broken">0%
+<tr><td> - <font color=lime>Process Target Effects</font><BR>&nbsp;&nbsp;&nbsp;Target Source Unknown at this time</td><td class="broken">0%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
 <tr><td style="text-align: left; height: 12px; width: 1%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
 <td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
 // -->
-<tr><td colspan=3><font color=white><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fx Target Processing - Some of these vary depending upon Fx source</h3></font></td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Self</font><BR>Source Item or Location for Group Effects</td><td class="working">80%
+<tr><td colspan=3><font color=white><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fx Target Processing - These vary depending on Source</h3></font></td></tr>
+<tr><td> - <font color=lime>Apply Processed Effects to Self</font><BR>&nbsp;&nbsp;&nbsp;Source Item or a Location for Group Effects</td><td class="working">95%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 80%; background:#00DB00"></td>
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Character</font><BR>Character or Character Skills</td><td class="working">95%
+<td><font color=pink>Final Testing</font><BR>17Feb20</td></tr>
+<tr><td> - <font color=lime>Apply Processed Effects to Character</font><BR>&nbsp;&nbsp;&nbsp;Character or Character Skills</td><td class="working">95%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
 <tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
 <td><font color=pink>Final Testing</font><BR>7Feb20</td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Ship</font><BR>Character's Ship</td><td class="working">80%
+<tr><td> - <font color=lime>Apply Processed Effects to Ship</font><BR>&nbsp;&nbsp;&nbsp;Character's Ship</td><td class="working">95%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 80%; background:#00DB00"></td>
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Target</font><BR>Ship's current target</td><td class="working">50%
+<td><font color=pink>Final Testing</font><BR>17Feb20</td></tr>
+<tr><td> - <font color=lime>Apply Processed Effects to Target</font><BR>&nbsp;&nbsp;&nbsp;Ship's current target</td><td class="working">85%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 50%; background:#00DB00"></td>
-<td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Working</font> - <font color=olive>Needs More Testing</font><BR>19Jun19</td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Other</font><BR>Character's ship for Skill Source.  Specific Module for Charge Source.</td><td class="working">80%
-<table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 80%; background:#00DB00"></td>
-<td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
-<td><font color=lime>Mostly Complete</font><BR><font color=orange>Testing</font><BR>19Jun19</td></tr>
-<tr><td> - <font color=lime>Apply Processed Effects to Charge</font><BR>Loaded Charge on a Module or Module Group</td><td class="working">50%
-<table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
-<tr><td style="text-align: left; height: 12px; width: 50%; background:#00DB00"></td>
+<tr><td style="text-align: left; height: 12px; width: 85%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
 <td><font color=lime>Working</font> - <font color=olive>Needs More Testing</font><BR>19Jun19</td></tr>
+<tr><td> - <font color=lime>Apply Processed Effects to Other</font><BR>&nbsp;&nbsp;&nbsp;Character's ship for Skill Source.  Specific Module for Charge Source.</td><td class="working">95%
+<table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
+<td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
+<td><font color=pink>Final Testing</font><BR>17Feb20</td></tr>
+<tr><td> - <font color=lime>Apply Processed Effects to Charge</font><BR>&nbsp;&nbsp;&nbsp;Loaded Charge on a Module or Module Group</td><td class="working">95%
+<table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
+<tr><td style="text-align: left; height: 12px; width: 95%; background:#00DB00"></td>
+<td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
+<td><font color=pink>Final Testing</font><BR>17Feb20</td></tr>
 <!--
-<tr><td> - <font color=lime>Apply Processed Effects to Area</font><BR>Area Target Unknown at this time</td><td class="broken">0%
+<tr><td> - <font color=lime>Apply Processed Effects to Area</font><BR>&nbsp;&nbsp;&nbsp;Area Target Unknown at this time</td><td class="broken">0%
 <table style="text-align: left; width: 150px; border: 1px solid black" cellspacing="1">
 <tr><td style="text-align: left; height: 12px; width: 1%; background:#00DB00"></td>
 <td style="text-align: left; height: 15px; background:#ffffff"></td></tr></table></td>
