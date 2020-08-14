3c9e14c4312e94e75f166ff39204d700f1996573
Allan <allan@dev.ke5jro.net>
2020-08-14 15:53:44 -0500

    version update - TODO update
    Signed-off-by: Allan <allan@dev.ke5jro.net>

--- src/eve-common/EVEVersion.h
+++ src/eve-common/EVEVersion.h
@@ -39,8 +39,8 @@ static const char* const EVEProjectCodename = "EVE-EVE-TRANQUILITY";
 static const int32 EVEBirthday = 170472;
 
 /*  Allan's Static Definitions */
-static const char* const EVEMU_REVISION = "0.65.21";
-static const char* const EVEMU_BUILD_DATE = "1 August 2020";
+static const char* const EVEMU_REVISION = "0.66.47";
+static const char* const EVEMU_BUILD_DATE = "12 August 2020";
 /* match versions here with stated files for full support */
 static const float Config_Version = 9.8; /* eve-server.xml and EveServerConfig.cpp */
 static const float Log_Version = 9.9;    /* logtypes.h and log.ini */
